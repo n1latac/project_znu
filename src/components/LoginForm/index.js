f87b6bd458c0482c21b8bf339943974d8d34b5e1
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2023-03-27 19:55:06 +0300

    add page login

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,92 +1,66 @@
 import React from 'react';
-import Header from '../Header/Header';
+import {Formik, Form, Field} from 'formik';
+import CustomField from '../CustomField';
+import { LoginSchema } from '../../validators/validationSchems';
 import styles from './LogInForm.module.css';
-import { Formik, Form, Field } from 'formik';
-import  {LoginSchema}  from '../../validators/validationSchems';
+import Header from '../Header/Header';
 
-export default function LogInForm() {
-
-    const clicked = () => {
-        console.log('hello')
+const Login = (props) => {
+    const initialState ={
+        email: '',
+        password: '',
+        rememberUser: false,
     }
-    
-  return (
-    <div className={styles['login-body']}>
-        <Header style={{padding: '0vh 10vw', margin: '0 auto', background: 'transparent', backdropFilter: 'blur(20px)'}}/>
-       <section className={styles['login-content']}>
-            <div className={styles[`login-box`]}>
-                <Formik
-                initialValues={{
-                    email: '',
-                    password: ''
-                }}
-                onSubmit={clicked()}
-                validationSchema={LoginSchema}
-                >
-                    {(formikProps) => {
-                        console.log(formikProps)
-                        return(  
-                     
-                   <Form autoComplete='off'>
-                        <h2>Login</h2>
-                        <div className={styles['login-inputbox']}>
-                        <ion-icon name="mail-outline"></ion-icon>
-                            <Field className={styles['login-input']} type='text' name='email' required placeholder=' '></Field>
-                            <label className={styles['login-label']}>Email</label>
+
+    const inputStyle = {
+        width: '100%',
+        padding: '0px 15px',
+        margin: '0px 0px 20px 0px',
+        fontSize: '16px',
+        color: 'white',
+        lineHeight: '3em',
+        borderRadius: '5px',
+        border: '1px solid white',
+        boxSizing: 'border-box',
+    }
+
+    const submitHandler = (values, actions) => {
+
+    }
+
+    return (
+        <section className={styles.background}>
+            <Header style={{padding: '0vh 10vw', margin: '0 auto', background: 'transparent', backdropFilter: 'blur(20px)'}}/>
+            <div className={styles['signup-content']}>
+            <div className={styles.container}>
+            <Formik
+            initialValues={initialState}
+            onSubmit={submitHandler}
+            validationSchema={LoginSchema}
+            >
+                {(formikProps)=>{
+                    console.log(formikProps)
+                    return( 
+                    <Form className={styles.form}>
+                        <p className={styles.title}>LOGIN TO YOUR ACCOUNT</p>
+                        <CustomField name='email' placeholder='Enter your email' style={inputStyle}/>
+                        <CustomField name='password' placeholder='Enter your password' style={inputStyle}/>
+                        <div className={styles.remind}>
+                            <label>
+                                <input type='checkbox'/>
+                                Remember Me
+                            </label>
+                            <a href='#'>Forgot Password</a>
                         </div>
-                        <div className={styles['login-inputbox']}>
-                        <ion-icon name="lock-closed-outline"></ion-icon>
-                            <Field className={styles['login-input']} type='password' name='password' required placeholder=' '></Field>
-                            <label>Password</label>
-                        </div>
-                        <div className={styles.forget}>
-                            <label><input type='checkbox'></input>Remember me<a href='#'>Forget Password</a></label>
-                        </div>
-                        <button className={styles['login-button']} type='submit'>Log in</button>
-                        <div className={styles.register}>
-                            <p>Don't have an account <a href='#'>Register</a></p>
-                        </div>
+                        <button type='submit' className={styles.loginButton}>LOGIN</button>
                     </Form>
-                    )}}
-                </Formik>
-                 <div>
-   </div>
+                    )
+                }}
+            </Formik>
+            </div>
             </div>
         </section>
-    </div>
-  )    
-}    
-    
-    
+    );
+}
 
-
-{/*
-<div className={styles['input-body']}>
-        <Header style={{padding: '0vh 10vw', margin: '0 auto', background: 'transparent', backdropFilter: 'blur(20px)'}}/>
-       <section className={styles['form-content']}>
-            <div className={styles[`form-box`]}>
-                <div>
-                    <form>
-                        <h2>Login</h2>
-                        <div className={styles.inputbox}>
-                        <ion-icon name="mail-outline"></ion-icon>
-                            <input classname={styles['login-input']}type='email' required placeholder=' '></input>
-                            <label className={styles['login-label']}>Email</label>
-                        </div>
-                        <div className={styles.inputbox}>
-                        <ion-icon name="lock-closed-outline"></ion-icon>
-                            <input className={styles['login-input']} type='pass' required placeholder=' '></input>
-                            <label>Password</label>
-                        </div>
-                        <div className={styles.forget}>
-                            <label><input type='checkbox'></input>Remember me<a href='#'>Forget Password</a></label>
-                        </div>
-                        <button>Log in</button>
-                        <div className={styles.register}>
-                            <p>Don't have an account <a href='#'>Register</a></p>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </section>
-    </div>*/}
+export default Login;
